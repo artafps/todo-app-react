402eb7ed72c0d1a5381becb866f61f698e1d5cad
artafallahpoor <artafallahpoor@gmail.com>
2022-10-11 23:00:30 +0330

    Updates

--- js/index.js
+++
@@ -1,5 +0,0 @@
-if(document.querySelector("#myModal")){
-document.querySelector("#myModal").addEventListener("shown.bs.modal",(function(){document.querySelector("#myInput").trigger("focus")}))
-}
-  
- 
